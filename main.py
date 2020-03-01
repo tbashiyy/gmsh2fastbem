d6fab383d4a570174f106c4d1490256fd132c10e
tbashiyy <t.bashiyy@gmail.com>
2020-03-01 11:48:39 +0900

    add typings

--- main.py
+++ main.py
@@ -1,55 +1,69 @@
 import numpy as np
-
-#読み込みファイルを指定
-f = open('./gmshSource/newMesh0223_meshsize01')
-
-nodes = []
-elements =  []
-nodesLength = 0
-elementsLength = 0
-
-line = f.readline()
-
-#Nodesまで行を飛ばす
-while line:
-    if(line == '$Nodes\n'):
-        line = f.readline()
-        nodesLength = int(line)
-        break
-    else:
-        line = f.readline()
+from typing import Tuple, List
 
 
-#カウント部分を飛ばすS
-line = f.readline()
+def main() -> None:
+    nodes, elements = readFile('./gmshSource/newMesh0223_meshsize01')
+    print(len(nodes))
+    print(len(elements))
 
-#Nodes読み込み
-while line:
-    if(line == '$EndNodes\n'):
-        line = f.readline()
-        break
-    else:
-        nodes.append(line)
-        line = f.readline()
+#読み込み
+def readFile(filename: str) -> Tuple[List[str], List[str]]:
+    f = open(filename)
 
-#カウント部分を飛ばす
-line = f.readline()
-elementsLength = int(line)
-line = f.readline()
+    nodes = []
+    elements =  []
+    nodesLength = 0
+    elementsLength = 0
 
-#Elements読み込み
-while line:
-    if(line == '$EndElements\n'):
-        break
-    else:
-        elements.append(line)
-        line = f.readline()
+    line = f.readline()
 
-#データ長チェック
-if(len(nodes) != nodesLength):
-    raise Exception('Illegal data length')
+    #Nodesまで行を飛ばす
+    while line:
+        if(line == '$Nodes\n'):
+            line = f.readline()
+            nodesLength = int(line)
+            break
+        else:
+            line = f.readline()
 
-if(len(elements) != elementsLength):
-    raise Exception('Illegal data length')
 
-f.close()
+    #カウント部分を飛ばすS
+    line = f.readline()
+
+    #Nodes読み込み
+    while line:
+        if(line == '$EndNodes\n'):
+            line = f.readline()
+            break
+        else:
+            nodes.append(line)
+            line = f.readline()
+
+    #カウント部分を飛ばす
+    line = f.readline()
+    elementsLength = int(line)
+    line = f.readline()
+
+    #Elements読み込み
+    while line:
+        if(line == '$EndElements\n'):
+            break
+        else:
+            elements.append(line)
+            line = f.readline()
+
+    #データ長チェック
+    if(len(nodes) != nodesLength):
+        raise Exception('Illegal data length')
+
+    if(len(elements) != elementsLength):
+        raise Exception('Illegal data length')
+    
+    f.close()
+
+    return nodes, elements
+
+
+#実行
+main()
